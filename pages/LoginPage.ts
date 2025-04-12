6fc2ae1d970abfce98affa3ceee48deda8d0904f
Vladyslava <71496479+Vladyslava95@users.noreply.github.com>
2025-04-12 16:00:28 +0300

    Unit 9 tests (#1)
    * Unit 9 tests
    
    * Fix comemnts
    
    * Fix comments

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,25 +1,40 @@
-import {Page, Locator} from "@playwright/test"
+import {Page, Locator, expect} from "@playwright/test"
 import { getAuthData } from '../utils';
 
 export class LoginPage {
     private email: Locator;
     private password: Locator;
+    private navMenu: Locator;
     private loginButton: Locator;
+    private pageTitle: Locator;
 
     public constructor (private page: Page ) {
         this.email = this.page.locator('[data-test="email"]');
         this.password = this.page.locator('[data-test="password"]');
         this.loginButton = this.page.locator('[data-test="login-submit"]');
+        this.navMenu = this.page.locator('[data-test="nav-menu"]');
+        this.pageTitle = page.locator('[data-test="page-title"]');;
     }
 
     async navigateTo() {
         await this.page.goto('/auth/login')
-    }
+    };
 
     async login(){
         const authData = getAuthData();
         await this.email.fill(authData.email);
         await this.password.fill(authData.password);
         await this.loginButton.click();
-    } 
+    };
+
+    async verifyAccountMenu(name: string) {
+        await expect(this.navMenu).toBeVisible();
+        await expect(this.navMenu).toContainText(name);
+    };
+
+    async verifyPageTitle(title: string) {
+        await expect(this.pageTitle).toBeVisible();
+        await expect(this.pageTitle).toContainText(title);
+    };
+    
 }
--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -2,82 +2,54 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
 import { ProductPage } from '../pages/ProductPage';
+import { CheckOutPage } from '../pages/CheckoutPage';
 
 test('Verify login with valid credentials', async ({ page }) => {
     const loginPage = new LoginPage(page);
+
     await loginPage.navigateTo();
     await loginPage.login();
-
-    await expect(page).toHaveURL('/account');
-    const pageTitle = page.locator('[data-test="page-title"]');
-    await expect(pageTitle).toBeVisible();
-    await expect(pageTitle).toContainText("My account");
-    const navMenu = page.locator('[data-test="nav-menu"]');
-    await expect(navMenu).toBeVisible();
-    await expect(navMenu).toContainText("Jane Doe");
+    expect(page).toHaveURL('/account');
+    await loginPage.verifyPageTitle("My account");
+    await loginPage.verifyAccountMenu("Jane Doe");
 
   });
 
 test('Verify user can view product details', async ({ page }) => {
     const homePage = new HomePage(page);
     const loginPage = new LoginPage(page);
+    const productPage = new ProductPage(page);
 
     await loginPage.navigateTo();
     await loginPage.login();
-    await expect(page).toHaveURL('/account');
-
+    expect(page).toHaveURL('/account');
     await homePage.openHomepage();
     await homePage.clickOnProduct('Combination Pliers');
-
-    await expect(page.url()).toContain('/product');
-    const productTitle = page.locator('[data-test="product-name"]');
-    await expect(productTitle).toBeVisible();
-    await expect(productTitle).toContainText("Combination Pliers");
-    const utitPrice = page.locator('[data-test="unit-price"]');
-    await expect(utitPrice).toBeVisible();
-    await expect(utitPrice).toContainText("14.15");
-    const addToCart = page.locator('[data-test="add-to-cart"]');
-    await expect(addToCart).toBeVisible();
-    const favorites = page.locator('[data-test="add-to-favorites"]');
-    await expect(favorites).toBeVisible();
-
-    
+    expect(page.url()).toContain('/product');
+    await productPage.verifyProductCard("Combination Pliers", "14.15")
 
   });
 
-  test('Verify user can add product to cart', async ({ page }) => {
+  test('Verify  add product to cart', async ({ page }) => {
     const homePage = new HomePage(page);
     const productPage = new ProductPage(page);
     const loginPage = new LoginPage(page);
+    const checkoutPage = new CheckOutPage(page)
 
     await loginPage.navigateTo();
     await loginPage.login();
-    await expect(page).toHaveURL('/account');
+    expect(page).toHaveURL('/account');
 
     await homePage.openHomepage();
     await homePage.clickOnProduct('Slip Joint Pliers');
-    await expect(page.url()).toContain('/product');
-    const productTitle = page.locator('[data-test="product-name"]');
-    await expect(productTitle).toBeVisible();
-    await expect(productTitle).toContainText("Slip Joint Pliers");
-    const utitPrice = page.locator('[data-test="unit-price"]');
-    await expect(utitPrice).toBeVisible();
-    await expect(utitPrice).toContainText("9.17");
+    expect(page.url()).toContain('/product');
 
+    await productPage.verifyProductCard("Slip Joint Pliers", "9.17")
     await productPage.addToCart();
-    const alert = page.getByRole('alert');
-    await expect(alert).toHaveText('Product added to shopping cart.');
-    await expect(alert).toBeHidden({ timeout: 8_000 });
-    const itemQty = page.locator('[data-test="cart-quantity"]');
-    await expect(itemQty).toHaveText('1');
+    await productPage.verifyAlertIsShown('Product added to shopping cart.');
+    await productPage.verifyIncreaseItemQty('1');
 
     await productPage.proceedToCheckout();
-    await expect(page).toHaveURL('/checkout');
-    const productName = page.locator('[data-test="product-title"]');
-    await expect(productName).toHaveText('Slip Joint Pliers');
-    const productQty = page.locator('[data-test="product-title"]');
-    await expect(productQty).toHaveCount(1);
-    const checkOutButton = page.locator('[data-test="proceed-1"]');
-    await expect(checkOutButton).toBeVisible();
-   
+    expect(page).toHaveURL('/checkout');
+    await checkoutPage.verifyProductCard('Slip Joint Pliers', 1 )
   });
