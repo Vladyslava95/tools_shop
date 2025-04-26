90160d3c3f61f56d082116ef7ee348f35682ffed
Vladyslava <71496479+Vladyslava95@users.noreply.github.com>
2025-04-26 11:30:13 +0300

    Sorting Test (#2)
    * Sorting Test
    
    * Fix comment

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,12 +1,17 @@
 import {Page, Locator} from "@playwright/test"
-
+import { SortOptions, SortComponent  } from "../components/sort.component";
 export class HomePage {
     
     private homeIcon: Locator;
-
+    private productName: Locator;
+    private productPrice: Locator;
+    private sortComponent: SortComponent;
 
     public constructor (private page: Page ) {
         this.homeIcon = this.page.locator('.navbar-brand');
+        this.productName = this.page.locator('[data-test="product-name"]');
+        this.productPrice = this.page.locator('[data-test="product-price"]');
+        this.sortComponent = new SortComponent(page);
     };
     
     async openHomepage() {
@@ -17,5 +22,20 @@ export class HomePage {
         await this.page.getByText(productName, { exact: true }).click({ timeout: 5000 });
       
     };
+
+    async sortProducts(sortOption: SortOptions) {
+        await this.sortComponent.selectSortOption(sortOption);
+    };
+
+    async getProductNames(): Promise<string[]> {
+         return await this.productName.allInnerTexts();
+    };
+
+    async getProductPrice(): Promise<number[]> {
+        const priceTexts = await this.productPrice.allInnerTexts();
+        return priceTexts.map(text => parseFloat(text.replace('$', '').trim()));
+
+   };
+
     
 }
